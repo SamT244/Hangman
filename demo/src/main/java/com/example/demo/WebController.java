717d4281d5f50ff4713b18cedc0ed129e293e925
CatsLoveGames <malhart139@gmail.com>
2026-02-20 18:01:12 -0600

    Mappings

--- demo/src/main/java/com/example/demo/WebController.java
+++ demo/src/main/java/com/example/demo/WebController.java
@@ -1,5 +1,27 @@
 package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class WebController {
-    
+    @GetMapping("/home")
+    public String home(Model model){
+        return "home";
+    }
+
+    @GetMapping("/newGame")
+    public String newGame(Model model){
+        return "newGame";
+    }
+
+    @GetMapping("/settings")
+    public String settings(Model model){
+        return "settings";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model){
+        return "profile";
+    }
 }
