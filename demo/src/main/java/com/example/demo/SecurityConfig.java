3db1c63b702ecfbdf14ab95c20a3b510686913d0
SamT244 <samuel.tenney@g.austincc.edu>
2026-03-06 17:51:55 -0600

    working

--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -2,14 +2,46 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
 import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/home", "/profile", "/leaderboard").permitAll();
+                auth.requestMatchers("/settings").hasRole("USER");
+                auth.requestMatchers("/admin").hasRole("ADMIN");
+            }).formLogin(form -> form
+                .defaultSuccessUrl("/home"))
+            .build();
+    };
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails Henry = User.withUsername("Henry")
+        .password("{noop}password")
+        .roles("USER")
+        .build();
+
+        UserDetails Mallory = User.withUsername("Mallory")
+        .password("{noop}password")
+        .roles("USER")
+        .build();
+
+        UserDetails Admin = User.withUsername("Admin")
+        .password("{noop}password")
+        .roles("ADMIN")
+        .build();
+
+        return new InMemoryUserDetailsManager(Henry, Mallory, Admin);
+    }
+}
