d62d46bcb39b6fbf793c622ece2cb370e2205246
SamT244 <samuel.tenney@g.austincc.edu>
2026-03-06 17:20:04 -0600

    Added imports

---
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.web.SecurityFilterChain;
