50a054c4e9f06c23e103d1ca524673a51c6dc059
SamT244 <samuel.tenney@g.austincc.edu>
2026-03-13 18:36:35 -0500

    fixed css

--- demo/src/main/java/com/example/demo/WebController.java
+++ demo/src/main/java/com/example/demo/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(Model model){
         return "home";
     }
